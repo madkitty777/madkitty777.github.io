360e26f9fb3dc9c9954b8fe1da7c9ed6bf64bf63
madkitty777 <madkitty777@proton.me>
2026-01-05 18:13:20 +1030

    Update oneko.js

--- oneko/oneko.js
+++ oneko/oneko.js
@@ -86,7 +86,7 @@
   };
 
   function init() {
-    let nekoFile = "oneko/oneko.gif"
+    let nekoFile = "/oneko/oneko.gif"
     const curScript = document.currentScript
     if (curScript && curScript.dataset.cat) {
       nekoFile = curScript.dataset.cat
